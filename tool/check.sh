ec4b5ad16e8dd28854dde8315411546559f02a0f
Radomir Epur <radomir9720@gmail.com>
2025-05-26 11:21:08 +0300

    tool: make tool/check.sh script to fail when coverage is less than 100

--- tool/check.sh
+++ tool/check.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+
+MIN_COVERAGE=100
+
 flutter pub get &&
 dart format --set-exit-if-changed lib test &&
 flutter analyze lib test &&
-flutter test --coverage --test-randomize-ordering-seed random
+dart run coverage:test_with_coverage --fail-under $MIN_COVERAGE
